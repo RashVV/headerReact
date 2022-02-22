7d34442edd600e87d002af6f9340351122a91eab
Vadim Ratushnyak <40869757+RashVV@users.noreply.github.com>
2022-02-22 23:38:20 +0200

    update navItem array for menu

--- navbar-dropdown/components/NavItems.js
+++ navbar-dropdown/components/NavItems.js
@@ -1,53 +1,138 @@
-export const navItems = [
-  {
-    id: 1,
-    title: "Home",
-    path: "./",
-    cName: "nav-item",
-  },
-  {
-    id: 2,
-    title: "Services",
-    path: "./services",
-    cName: "nav-item",
-  },
-  {
-    id: 3,
-    title: "Products",
-    path: "./products",
-    cName: "nav-item",
-  },
-  {
-    id: 4,
-    title: "Contact Us",
-    path: "./contactus",
-    cName: "nav-item",
-  },
-];
+    export const navItems = [
+            {
+            id: 1,
+            title: 'Premieres',
+            path: './movies',
+            cName: 'nav-item',
+            },
+            {
+            id: 2,
+            title: 'All movies',
+            path: './R2D2',
+            cName: 'nav-item',
+            },
+            {
+            id: 3,
+            title: 'TV series',
+            path: './shows',
+            cName: 'nav-item',
+            },
+            {
+            id: 4,
+            title: 'Tape',
+            path: './feed',
+            cName: 'nav-item',
+            },
+            {
+                id: 5,
+                title: 'Videos',
+                path: './videos',
+                cName: 'nav-item',
+            },
+            {
+                id: 6,
+                title: 'News',
+                path: './news',
+                cName: 'nav-item',
+                
+            },
+        ];
 
-export const serviceDropdown = [
-  {
-    id: 1,
-    title: "Marketing",
-    path: "./marketing",
-    cName: "submenu-item",
-  },
-  {
-    id: 2,
-    title: "Consulting",
-    path: "./consulting",
-    cName: "submenu-item",
-  },
-  {
-    id: 3,
-    title: "Design",
-    path: "./design",
-    cName: "submenu-item",
-  },
-  {
-    id: 4,
-    title: "Development",
-    path: "./development",
-    cName: "submenu-item",
-  },
-];
+    export const PremieresDropdown = [
+            {
+                id: 1,
+                title: 'In the movies',
+                path: './cinema',
+                cName: 'mov__submenu-item',
+            },
+            {
+                id: 2,
+                title: 'Online',
+                path: './online',
+                cName: 'mov__submenu-item',
+            },
+            {
+                id: 3,
+                title: 'Soon',
+                path: './premier',
+                cName: 'mov__submenu-item',
+            },
+            ];
+    
+    export const AllmuviesDropdown = [
+            {
+                id: 1,
+                title: 'Search movies',
+                path: './R2D2',
+                cName: 'search__submenu-item',
+            },
+            {
+                id: 2,
+                title: 'Collections',
+                path: './collections/moviroomapp',
+                cName: 'search__submenu-item',
+            },
+            {
+                id: 3,
+                title: 'Recommendations',
+                path: './recommendations/movies',
+                cName: 'search__submenu-item',
+            },
+            {
+                id: 4,
+                title: 'Search by name',
+                path: './search/names',
+                cName: 'search__submenu-item',
+            },
+         ];
+        
+        export const TVseriesDropdown = [
+            {
+                id: 1,
+                title: 'Popular',
+                path: './popular',
+                cName: 'show__submenu-item',
+              },
+              {
+                id: 2,
+                title: 'My series',
+                path: './my',
+                cName: 'show__submenu-item',
+              },
+              {
+                id: 3,
+                title: 'My calendars',
+                path: './calendar',
+                cName: 'show__submenu-item',
+              },    
+            ];
+        
+        export const TapeDropdown = [
+            {
+                id: 1,
+                title: 'Friends',
+                path: './friends',
+                cName: 'feed__submenu-item',
+              },
+              {
+                id: 2,
+                title: 'Favorites',
+                path: './favorites',
+                cName: 'feed__submenu-item',
+              },
+            ];
+       
+        export const VideosDropdown = [
+            {
+                id: 1,
+                title: 'Trailers',
+                path: './trailers',
+                cName: 'videos__submenu-item',
+              },
+              {
+                id: 2,
+                title: 'Reviews',
+                path: './favorites',
+                cName: 'videos__submenu-item',
+              },
+            ];
